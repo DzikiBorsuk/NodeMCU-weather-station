69142e077edac03d3f8cb89d6bde4477f5750d23
Piotr <piotrmeller95@gmailc.om>
2018-06-21 11:54:20 +0200

    init

---
+++ weatherStation/weatherStation/weatherStation.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		weatherStation.ino
+ Created:	21.06.2018 11:52:42
+ Author:	Piotr Meller
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
